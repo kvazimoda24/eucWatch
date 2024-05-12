d538928c8ffc0c063b9769d54c611b0e6f36d9e0
Igor Vasilkov <igor@kvazimoda.pro>
2024-05-12 21:02:23 +0300

    Fix init

--- v2/init/init.js
+++ v2/init/init.js
@@ -48,10 +48,9 @@ if ((BTN1.read() || require("Storage").read("devmode")) && process.env.BOARD != 
   }
   else if (mode == "shutdown") {
     digitalPulse(ew.pin.BUZZ, ew.pin.BUZ0, 300);
-    var i2c = new I2C();
-    i2c.setup({ scl: "D7", sda: "D6", bitrate: 100000 });
-    digitalPulse("D13",1,[5,50]);i2c.writeTo(0x15,0xE5,0x03); // Touch off
-    i2c.writeTo(0x18,0x20,0x08); // ACC low-power
+    E.enableWatchdog(process.env.BOARD == "DSD6"?2:600, true);
+    clearInterval(wdint);
+    //wdint = setInterval(KickWd, process.env.BOARD == "DSD6"?1000:300000);
     NRF.sleep();
   }
   else {
